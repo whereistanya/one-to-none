626497bccfa2344583e2bcebb63371f4f5d5841c
Tanya Reilly <tanya.reilly@gmail.com>
2017-01-11 23:32:21 -0500

    Clean up indentation. I need to fix my .vimrc.

--- content.js
+++ content.js
@@ -13,7 +13,7 @@ function checkEvent() {
   if(!location.hash.match(/^#eventpage_6/)) return;  // the 'create' page.
 
   var meeting_title = $(".ep-title > input").val();
-	$(".ep-title > input").on('change', function() {
+  $(".ep-title > input").on('change', function() {
     console.log("Meeting title changed to:", this.value);
     meeting_title = this.value;
   });
@@ -21,26 +21,26 @@ function checkEvent() {
   // Check for attendees when the Save button is clicked.
   $("div[id*='.save_top'].action-btn-wrapper").click(function() {
 
-		console.log("Meeting title is", meeting_title);
-		var titleRE = /([\w\.]+):([\w\.]+)/g;
-		var match = titleRE.exec(meeting_title);
-		if (!match || match.length !== 3) {
+    console.log("Meeting title is", meeting_title);
+    var titleRE = /([\w\.]+):([\w\.]+)/g;
+    var match = titleRE.exec(meeting_title);
+    if (!match || match.length !== 3) {
       console.log("Not a 1:1");
       return;
     }
     console.log("Looks like a 1:1 between", match[1], "and", match[2]);
 
-		var guestList = $(".ep-gl-guest");
+    var guestList = $(".ep-gl-guest");
 
     var invitednames = []
-		for (i = 0; i < guestList.length; i++) {
-			// TODO: This is hacky. Better to pull out the email address and parse properly.
-			console.log("invited", guestList[i]["title"], ",", guestList[i]["id"]);
-			invitednames.push(guestList[i]["title"])
-			invitednames.push(guestList[i]["id"])
-		}
+    for (i = 0; i < guestList.length; i++) {
+      // TODO: This is hacky. Better to pull out the email address and parse properly.
+      console.log("invited", guestList[i]["title"], ",", guestList[i]["id"]);
+      invitednames.push(guestList[i]["title"])
+      invitednames.push(guestList[i]["id"])
+    }
 
-		console.log("Logged in user is:", logged_in_user, "(" + logged_in_email + ")");
+    console.log("Logged in user is:", logged_in_user, "(" + logged_in_email + ")");
 
     for (i = 1; i <= 2; i++) {
       var invitee = match[i];
